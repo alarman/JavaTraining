90128de9420b7cf83eef57fcb3dbf26353d3cc42
Arman <armaniutian@gmail.com>
2024-05-18 21:18:35 +0600

    Submit 2nd assignment

---
+++ src/class2/PersonTest.java
@@ -0,0 +1,25 @@
+package class2;
+
+import class3.Person;
+
+public class PersonTest {
+    public static void main(String[] args) {
+        Person hasibul=new Person();
+        hasibul.name="Hasibul";
+        hasibul.gender="Male";
+        hasibul.height=5.7f;
+
+        hasibul.speak();
+        hasibul.work();
+
+        Person kobir=new Person();
+        kobir.name="Kobir";
+        kobir.gender="Male";
+        kobir.height=5.2f;
+
+        Person mili=new Person();
+        mili.name="Mili";
+        mili.gender="Female";
+        mili.height=4.2f;
+    }
+}
